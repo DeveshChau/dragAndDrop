38b8176cca601fa06d044f5e2940d0e6eed2b782
DeveshChau <deveshachaudhari@gmail.com>
2020-08-25 10:07:06 +0530

    render using OOP approach

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,17 @@
-// Code goes here!
+class ProjectTemplate {
+    templateElement: HTMLTemplateElement;
+    hostElement: HTMLDivElement;
+    element: HTMLFormElement;
+    constructor() {
+        this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
+        this.hostElement = document.getElementById('app')! as HTMLDivElement;
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild! as HTMLFormElement;
+        this.attach();
+    }
+    private attach() {
+        this.hostElement.insertAdjacentElement('afterbegin', this.element);
+    }
+}
+
+const prjTemplate = new ProjectTemplate();
