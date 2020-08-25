04396e2c8caced70054f50b95107aa4c6d636de8
DeveshChau <deveshachaudhari@gmail.com>
2020-08-25 10:33:54 +0530

    interacting with DOM elements

--- src/app.ts
+++ src/app.ts
@@ -2,15 +2,31 @@ class ProjectTemplate {
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
     element: HTMLFormElement;
+    title: HTMLInputElement;
+    description: HTMLInputElement;
+    people: HTMLInputElement;
     constructor() {
         this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
         this.hostElement = document.getElementById('app')! as HTMLDivElement;
         const importedNode = document.importNode(this.templateElement.content, true);
         this.element = importedNode.firstElementChild! as HTMLFormElement;
+        this.element.id = 'user-input';
+        this.title = document.querySelector('#title') as HTMLInputElement;
+        this.description = document.querySelector('#description') as HTMLInputElement;
+        this.people = document.querySelector('#people') as HTMLInputElement;
         this.attach();
+        this.configure();
     }
     private attach() {
         this.hostElement.insertAdjacentElement('afterbegin', this.element);
+    }
+    private handleEvent(event: Event) {
+        event.preventDefault();
+        console.log(this.title.value);
+        
+    }
+    private configure() {
+        this.element.addEventListener('submit', this.handleEvent)
     }
 }
 
