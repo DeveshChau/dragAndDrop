9b93abec787b6410eee005d897e7b95037321ed1
DeveshChau <deveshachaudhari@gmail.com>
2020-08-25 13:42:25 +0530

    reuseable validation function

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,31 @@
+interface Validable {
+    value: string | number;
+    required?: boolean;
+    maxLength?: number;
+    minLength?: number;
+    min?: number;
+    max?: number;
+}
+// validation function
+function validate(input: Validable) {
+    let isValid = true;
+    if (input.required) {
+        isValid = isValid && input.value.toString().trim().length !== 0;
+    }
+    if (input.maxLength != null && typeof input.value === 'string') {
+        isValid = isValid && input.value.length < input.maxLength;
+    }
+    if (input.minLength != null && typeof input.value === 'string') {
+        isValid = isValid && input.value.length > input.minLength;
+    }
+    if (input.min != null && typeof input.value === 'number') {
+        isValid = isValid && input.value > input.min;
+    }
+    if (input.max != null && typeof input.value === 'number') {
+        isValid = isValid && input.value < input.max;
+    }
+    return isValid;
+}
 // autobind decorator
 function Autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
     const originalMethod = descriptor.value;
@@ -36,7 +64,20 @@ class ProjectTemplate {
         const titleInput = this.title.value;
         const descriptionInput = this.description.value;
         const peopleInput = this.people.value;
-        if (titleInput.trim().length === 0 || descriptionInput.trim().length === 0 || peopleInput.trim().length === 0) {
+        const validateTitle: Validable = {
+            value: titleInput,
+            required: true
+        }
+        const validateDescription: Validable = {
+            value: descriptionInput,
+            minLength: 5,
+        }
+        const validatePeople: Validable = {
+            value: +peopleInput,
+            max: 5,
+            min: 1
+        }
+        if (!validate(validateTitle) || !validate(validateDescription) || !validate(validatePeople)) {
             alert('Inter proper value');
             return;
         } else {
